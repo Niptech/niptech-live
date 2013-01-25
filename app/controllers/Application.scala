889ae85fd2daf18b5f38955d0249428f8b272154
Fabrice Croiseaux <fcroiseaux@gmail.com>
2013-01-25 23:46:23 +0100

    Gravatar added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,6 +12,8 @@ import models._
 
 import akka.actor._
 import scala.concurrent.duration._
+import java.security.MessageDigest
+
 
 object Application extends Controller {
 
@@ -35,9 +37,9 @@ object Application extends Controller {
   /**
    * Display the chat room page.
    */
-  def chatRoom(username: Option[String]) = Action { implicit request =>
+  def chatRoom(username: Option[String], email: Option[String]) = Action { implicit request =>
     username.filterNot(_.isEmpty).map { username =>
-      Ok(views.html.chatRoom(username))
+      Ok(views.html.chatRoom(username, email.map(s=>md5SumString(s))))
     }.getOrElse {
       Redirect(routes.Application.index).flashing(
         "error" -> "Please choose a valid username.")
@@ -48,9 +50,16 @@ object Application extends Controller {
    * Handles the chat websocket.
    */
   def chat(username: String) = WebSocket.async[JsValue] { request =>
-
     ChatRoom.join(username)
+  }
 
+  def md5SumString(msg: String): String = {
+    val bytes = msg.getBytes
+    val md5 = MessageDigest.getInstance("MD5")
+    md5.reset()
+    md5.update(bytes)
+
+    md5.digest().map(0xFF & _).map { "%02x".format(_) }.foldLeft("") { _ + _ }
   }
 
 }
